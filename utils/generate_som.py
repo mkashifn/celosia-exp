3fe46b34e8aedcebf1876e28ca2da4dc9365c391
Kashif Naveed <mkashifn@gmail.com>
2020-01-19 20:02:54 +1100

    Add function to process selective data.

--- utils/generate_som.py
+++ utils/generate_som.py
@@ -4,9 +4,22 @@ from src.nbaiot import NbaIoT_Dataset
 from src.som import Som
 from utils.fetch_data import fetch_data_from_source_list
 
+def select_data(source_list):
+  selected_source_list = []
+  for source in source_list:
+    if source['normal']:
+      source['rows'] = 4000
+    else:
+      source['rows'] = 50
+
+    selected_source_list.append(source)
+  return selected_source_list
+
 def fetch_device_data(name):
   nbaiot = NbaIoT_Dataset()
   source_list = nbaiot.get_source_list_by_name(name)
+  source_list = select_data(source_list)
+
   (X, y) = fetch_data_from_source_list(source_list)
   print (X, y)
   print (X.shape, y.shape)
